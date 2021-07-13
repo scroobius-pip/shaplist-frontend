dc4728448386855396fc551ebce985560b88b0dd
simdi jinkins <sim04ful@gmail.com>
2021-07-13 09:50:48 +0000

    fixed google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Script from 'next/script'
+import Head from 'next/head'
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
-  <Script src='https://www.googletagmanager.com/gtag/js?id=G-ZC04SLV29Y'/>
-  <Script>
-    {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-ZC04SLV29Y');
-    `}
-  </Script>
+  <Head>
+
+  <script async src='https://www.googletagmanager.com/gtag/js?id=G-ZC04SLV29Y'/>
+  <script dangerouslySetInnerHTML={{
+    __html:`  window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    
+    gtag('config', 'G-ZC04SLV29Y');`
+  }}/>
+ 
+  </Head>
   <Component {...pageProps} />
   </>
 }
