a1b3ce30ca549a0891e9e299811498158b6394b7
simdi jinkins <sim04ful@gmail.com>
2021-07-13 10:18:29 +0000

    minor visual fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import illustration from '../public/storefront_illustration.jpg'
 import logo from '../public/storefront_shaplist_logo.svg'
-import { Button, Pane, Text, majorScale, Heading, ArrowRightIcon, UnorderedList, ListItem, TickIcon } from 'evergreen-ui'
+import { Button, Pane, Text, majorScale, Heading,Paragraph, ArrowRightIcon, UnorderedList, ListItem, TickIcon } from 'evergreen-ui'
 import React from 'react'
 import ComingSoonForm from '../components/ComingSoonForm'
 
@@ -11,17 +11,17 @@ export default function Home() {
   return (
     <>
       <Pane className={styles.container}>
-        <Pane><Image src={logo} /></Pane>
+        <Pane marginBottom={majorScale(4)}><Image src={logo} /></Pane>
         <Pane className={styles.top}>
           <Pane>
-            <Heading size={900} marginBottom={majorScale(2)}>
+            <Heading size={900} >
               <strong>Create a Web Page For Your Food Business</strong>
             </Heading>
-            <Heading size={700} marginBottom={majorScale(4)}>
-              Create an effective catalog page for your food business in 20 minutes without technical skills.
-            </Heading>
+            <Paragraph paddingTop={majorScale(2)} size={500} marginBottom={majorScale(2)}>
+              Create an effective catalog page for your food business in 20 minutes without any technical skills.
+            </Paragraph>
             <ComingSoonForm />
-            <Pane padding={majorScale(2)}>
+            <Pane padding={majorScale(2)} marginTop={majorScale(2)}>
               <Heading textAlign={'left'}><strong>Shaplist allows you to:</strong></Heading>
               <UnorderedList size={500} textAlign={'left'} icon={TickIcon} iconColor='success'>
                 <ListItem>Increase sales</ListItem>
