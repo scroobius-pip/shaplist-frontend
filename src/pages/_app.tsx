79dcd96b8f51c537a84694ce2c9cda3b8ab0c36a
simdi jinkins <sim04ful@gmail.com>
2021-07-13 09:41:48 +0000

    added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import Script from 'next/script'
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <Script src='https://www.googletagmanager.com/gtag/js?id=G-ZC04SLV29Y'/>
+  <Script>
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-ZC04SLV29Y');
+    `}
+  </Script>
+  <Component {...pageProps} />
+  </>
 }
 export default MyApp
