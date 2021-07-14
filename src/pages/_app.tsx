41a767433afe5be82e233ab421fe0e59664db6c8
simdi jinkins <sim04ful@gmail.com>
2021-07-14 20:04:25 +0100

    added facebook pixel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,20 @@ function MyApp({ Component, pageProps }: AppProps) {
   <Head>
 
   <script async src='https://www.googletagmanager.com/gtag/js?id=G-ZC04SLV29Y'/>
+    <script dangerouslySetInnerHTML={{
+      __html:`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+ fbq('init', '199928112062800'); 
+fbq('track', 'PageView');`
+      }}
+      
+      />
   <script dangerouslySetInnerHTML={{
     __html:`  window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
