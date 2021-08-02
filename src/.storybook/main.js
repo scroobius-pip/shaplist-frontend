e1e0943e307a8f8f4fd8c839845d69c168da291d
scroobius-pip <sim04ful@gmail.com>
2021-08-02 08:34:35 +0100

    fixed path aliasing issue with storybook

--- src/.storybook/main.js
+++ src/.storybook/main.js
@@ -1,3 +1,6 @@
+const path = require("path")
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
@@ -6,5 +9,14 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: (config) => {
+    config.resolve.plugins = config.resolve.plugins || [];
+    config.resolve.plugins.push(
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../tsconfig.json"),
+      })
+    );
+    return config
+  }
 }
