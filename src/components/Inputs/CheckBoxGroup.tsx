700014712613da0be3bfff949b67bad8e4e8a556
scroobius-pip <sim04ful@gmail.com>
2021-08-14 13:04:51 +0100

    updated checkboxgroup with a select all switch

--- src/components/Inputs/CheckBoxGroup.tsx
+++ src/components/Inputs/CheckBoxGroup.tsx
@@ -1,5 +1,7 @@
-import { Checkbox } from 'evergreen-ui'
+import Text, { No } from 'components/Text'
+import { Checkbox, Switch, majorScale } from 'evergreen-ui'
 import React from 'react'
+import CustomLabel from './Label'
 interface Props {
     options: string[]
     onChange: (options: string[]) => any
@@ -9,6 +11,25 @@ interface Props {
 
 const CheckBoxGroup = ({ options, onChange, value = [] }: Props) => {
     return <>
+
+        <CustomLabel description='Select all'  />
+        <Switch
+            hasCheckIcon
+            height={majorScale(3)}
+            checked={value.length === options.length}
+            onChange={(e) => {
+                switch (e.target.checked) {
+                    case true:
+                        onChange(options)
+                        break;
+
+                    case false:
+                        onChange([])
+                        break;
+                }
+            }}
+            marginBottom={majorScale(1)}
+        />
         {options.map(option => {
             return <Checkbox marginTop={0} key={option} value={option} checked={value.includes(option)} label={option} onChange={(e) => {
 
