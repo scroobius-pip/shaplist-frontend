77af6cab2e68c7f4e6645305da877224520595a5
scroobius-pip <sim04ful@gmail.com>
2021-08-23 18:05:46 +0100

    imageinput done

---
+++ src/components/Inputs/ImageInput.tsx
@@ -0,0 +1,106 @@
+import { AddIcon, Button, EditIcon, majorScale, Pane, PlusIcon, Spinner, TrashIcon } from 'evergreen-ui'
+import React from 'react'
+import ImageUploading, { ImageListType, ImageType } from "react-images-uploading";
+
+import { useState } from 'react'
+import { No } from 'components/Text';
+import delay from 'utils/delay';
+
+interface Props {
+    defaultValue?: string
+    value: ImageListType
+    onChange: (images: ImageListType) => any
+    // initialImage: string
+}
+interface ImagePreviewProps {
+    url: string;
+    loading?: boolean
+    onEdit?: (() => void) | undefined;
+    onDelete: (() => void) | undefined;
+}
+const ImagePreview = ({ url, loading, onEdit, onDelete }: ImagePreviewProps) => {
+    return <Pane paddingY={majorScale(1)} >
+        <Pane>
+            {loading ? <Spinner
+
+            /> : <img style={{ aspectRatio: '1 / 1', objectFit: 'cover' }} width='100%' src={url} />}
+        </Pane>
+        <Pane display='flex' flexDirection='row'>
+            {onEdit && <Button width='100%' onClick={onEdit} iconAfter={EditIcon} marginRight={majorScale(1)}>Update</Button>}
+            <Button width='100%' intent='danger' iconAfter={TrashIcon} onClick={onDelete}>Remove</Button>
+        </Pane>
+    </Pane>
+}
+
+
+const ImageInput = ({ onChange, value, defaultValue }: Props) => {
+  
+    const [show, setShow] = useState(!!defaultValue?.length)
+
+    if (show) {
+        return <ImagePreview
+            url={defaultValue ?? ''}
+            onDelete={() => {
+                setShow(false)
+                onChange([])
+            }}
+        />
+    }
+
+
+    return <Pane marginTop={majorScale(1)}>
+        <ImageUploading
+            value={value}
+            onChange={(files) => {
+                onChange(files)
+            }}
+            acceptType={["jpg", "png", "jpeg", "webp"]}
+            maxFileSize={5 * 1024 * 1024}
+        >
+            {({ imageList, onImageUpload, errors, onImageUpdate, onImageRemove }) => {
+
+                const RenderErrors = () => {
+                    return <Pane >
+
+                        {errors?.acceptType &&
+                            <span>Your selected file type is not allowed</span>}
+                        {errors?.maxFileSize && <No color='red'>Selected file size exceeds 5mb</No>}
+                    </Pane>;
+                };
+
+                return <Pane>
+                    <Pane>
+                        {!imageList.length && <Button
+                            iconAfter={PlusIcon}
+                            onClick={onImageUpload}
+
+                        >Select Image</Button>}
+                        <Pane
+                        >
+                            {(imageList ?? []).map((image, index) =>
+                                <ImagePreview
+                                    key={image.key}
+                                    url={image?.dataURL ?? ''}
+                                    onEdit={() => onImageUpdate(index)}
+                                    onDelete={() => onImageRemove(index)}
+                                />
+                            )}
+                        </Pane>
+                    </Pane>
+                    {hasError(errors) && <RenderErrors />}
+                </Pane>;
+            }}
+        </ImageUploading>
+    </Pane>
+}
+
+function hasError(obj: any) {
+    for (var key in obj) {
+        if (obj[key] === true) return true;
+    }
+    return false;
+}
+
+
+export default ImageInput
+
